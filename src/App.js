02105f49cd60edfce4881f900d7b986808dfbfdc
Prashani <aruniprashani@gmail.com>
2020-04-09 15:01:21 +0530

    create basic component of this app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Todos from './Todos'
 
-function App() {
-  return (
-    <div className="App" >
-      
-    </div>
-  );
+class App extends Component{
+
+  state = {
+    todos:[
+      {id: 1, content: 'buy some milks'},
+      {id: 2, content: 'play games'}
+    ]
+  } 
+  render() {
+    return (
+      <div className="App container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos}/>
+      </div>
+    );
+  }
 }
-
 export default App;
